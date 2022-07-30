94ae435e495b08fa49756f5b99e456edb29d5213
AhmedNabilTopaz <60816134+AhmedNabilTopaz@users.noreply.github.com>
2022-07-30 19:27:43 +0200

    fix report issues

--- accounting_topaz/models/purchase_report.py
+++ accounting_topaz/models/purchase_report.py
@@ -15,9 +15,16 @@ _logger = logging.getLogger(__name__)
 
 class PurchaseReport(models.Model):
     _inherit = "purchase.report"
-    z_package_barcode = fields.Char('Full Barcode', related='product_tmpl_id.z_package_barcode', store=True)
+    # z_package_barcode = fields.Char('Full Barcode', related='product_tmpl_id.z_package_barcode', store=True)
     # z_package_barcode= fields.Many2one('product.template','Full Barcode',readonly=True)
+    z_package_barcode = fields.Char('Full Barcode', compute='_getdata', store=True)
 
+    def _getdata(self):
+        sql = """select z_package_barcode from product_template where id=%s"""
+        self.env.cr.execute(sql, self.product_tmpl_id)
+        for rec in self.env.cr.fetchall():
+            self.z_package_barcode = rec[0]
+            print("Name is", rec[0])
     def _select(self):
         return super(PurchaseReport,
                      self)._select() + ", t.z_package_barcode as z_package_barcode"
